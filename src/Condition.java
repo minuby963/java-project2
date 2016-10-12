c2f3fb5c7041e056ec3399bd0544b460a1a773d4
minuby963 <minuby369@gmail.com>
2016-10-13 00:23:40 +0200

    Individual class

---
+++ src/Condition.java
@@ -0,0 +1,52 @@
+public class Condition {
+	private int condition;
+	/*
+	 * 0 - dead
+	 * 1 - alive
+	 * 2 - starved
+	 * 
+	 * 
+	 * 
+	 */
+	public Condition(){
+		condition = 1;
+	}
+	public Condition(Condition con){
+		condition = con.getConditionSymbol();
+	}
+	public Condition(String con){
+		switch(con.toLowerCase()){
+		case "dead":
+			condition = 0;
+			break;
+		case "alive":
+			condition = 1;
+			break;
+		case "starved":
+			condition = 2;
+			break;
+		default:
+			condition = 3;
+			break;
+		}
+	}
+	public int getConditionSymbol(){
+		return condition;
+	}
+	public String getConditionName(){
+		switch(condition){
+		case 0:
+			return "dead";
+		case 1:
+			return "alive";
+		case 2:
+			return "starved";
+		default:
+			return "Unknown";
+		}
+	}
+	@Override
+	public String toString(){
+		return "condition: "+getConditionName()+" \n";
+	}
+}
